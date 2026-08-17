0f19ad6d3ec815410d5a4e5fbcce8d3909197f16
Shi Boyu <e1714037@u.nus.edu>
2026-08-17 11:21:16 +0800

    Level-0: Rename chatbot to Morgan and set initial greet/exit text

---
+++ src/main/java/Morgan.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Morgan {
+    private static final String DIVIDER = "____________________________________________________________";
+    private static final String CHATBOT_NAME = "Morgan";
+    private static final String BANNER = " __  __                            \n"
+            + "|  \\/  | ___  _ __ __ _  __ _ _ __  \n"
+            + "| |\\/| |/ _ \\| '__/ _` |/ _` | '_ \\ \n"
+            + "| |  | | (_) | | | (_| | (_| | | | |\n"
+            + "|_|  |_|\\___/|_|  \\__, |\\__,_|_| |_|\n"
+            + "                  |___/             \n";
+    public static void main(String[] args) {
+        System.out.println(DIVIDER);
+        System.out.println(BANNER);
+        System.out.println(DIVIDER);
+        System.out.println("Meow~ I'm " + CHATBOT_NAME + "\uD83D\uDC3E");
+        System.out.println("What can I do for you? Meow~");
+        System.out.println(DIVIDER);
+        System.out.println("Meow~ Bye bye, human! Don't forget to feed me~");
+        System.out.println(DIVIDER);
+    }
+}
