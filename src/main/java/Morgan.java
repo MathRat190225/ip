e2232bd62f96a76eca1f308dcf992b2ace3fa6d6
Shi Boyu <e1714037@u.nus.edu>
2026-08-17 13:13:30 +0800

    Level-2: Implement add/list function

--- src/main/java/Morgan.java
+++ src/main/java/Morgan.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class Morgan {
     private static final String DIVIDER = "____________________________________________________________";
     private static final String CHATBOT_NAME = "Morgan";
-    private static final String BANNER = " __  __                            \n"
+    private static final String BANNER =
+            " __  __                            \n"
             + "|  \\/  | ___  _ __ __ _  __ _ _ __  \n"
             + "| |\\/| |/ _ \\| '__/ _` |/ _` | '_ \\ \n"
             + "| |  | | (_) | | | (_| | (_| | | | |\n"
             + "|_|  |_|\\___/|_|  \\__, |\\__,_|_| |_|\n"
             + "                  |___/             \n";
+    private static final String[] tasks = new String[100];
+    private static int taskCount = 0;
+
     public static void main(String[] args) {
         //1.Greet
         System.out.println(DIVIDER);
@@ -25,17 +29,30 @@ public class Morgan {
             String input = sc.nextLine();
 
             //exit
-            if (input.equals("bye")) {
+            if (input.equalsIgnoreCase("bye")) {
                 break;
             }
 
             //echo
             System.out.println(DIVIDER);
-            System.out.println(" " + input + " \uD83D\uDC3E");
-            System.out.println(DIVIDER);
+
+            if (input.equalsIgnoreCase("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.printf(" \uD83C\uDFA3%d.%s\n", i + 1, tasks[i]);
+                }
+            } else {
+                if (taskCount < tasks.length) {
+                    tasks[taskCount] = input;
+                    taskCount++;
+                    System.out.println(" \uD83D\uDC3Eadded: " + input);
+                } else {
+                    System.out.println("Meow! That's too much for me!(Maximum: 100)");
+                }
+            }
         }
 
         //3.Exit
+        System.out.println(DIVIDER);
         System.out.println(" Meow~ Bye bye, human! Don't forget to feed me~");
         System.out.println(DIVIDER);
 
